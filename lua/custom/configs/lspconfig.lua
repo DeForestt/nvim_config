2ac3383ff2ec692722473216a3100b4f242b4893
= <deforesttthompson@gmail.com>
2025-02-24 09:35:59 -0500

    update to tsls

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -23,7 +23,7 @@ lspconfig.rust_analyzer.setup({
   capabilities = capabilities,
 })
 
-lspconfig.tsserver.setup({
+lspconfig.ts_ls.setup({
   on_attach = on_attach,
   capabilities = capabilities,
 })
